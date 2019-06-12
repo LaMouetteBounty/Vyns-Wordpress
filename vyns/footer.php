3363476c95c4c50f411e54e3b72c45444e46ad47
LaMouetteBounty <la-lilouche@hotmail.fr>
2019-06-12 12:01:55 +0200

    pause midi footer en cours

--- vyns/footer.php
+++ vyns/footer.php
@@ -1,10 +1,47 @@
-</div> <!-- /.container -->
-<footer class="blog-footer">
-<p>Par <a href="https://www.wppourlesnuls.com/">WP Pour les Nuls</a>.</p>
+<footer class="container-fluid">
+    <div class="container ">
+        <div class="row content_footer">
+
+            <div class="copyright">
+                <p>&copy; Vyns </p>
+            </div>
+
+            <div class="menu_footer">
+                <ul>
+                    <li>
+                        <a href="#">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#">ACTUALITÉS</a>
+                    </li>
+                    <li>
+                        <a href="#">BLUES</a>
+                    </li>
+                    <li>
+                        <a href="#">DISCO</a>
+                    </li>
+                    <li>
+                        <a href="#">ROCK</a>
+                    </li>
+                    <li>
+                        <a href="#">JAZZ</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="reseaux">
+                <img src="http://localhost:8080/wp-content/themes/vyns/assets/images/reseaux/facebook.png" width="20">
+                <img src="http://localhost:8080/wp-content/themes/vyns/assets/images/reseaux/insta.png" width="20">
+                <img src="http://localhost:8080/wp-content/themes/vyns/assets/images/reseaux/twitter.png" width="20">
+            </div>
+
+        </div>
+    </div>
 </footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 </body>
+
 </html>
